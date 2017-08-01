66d74a79ea7c5ed3f6b86b6a03c4d31159054e64
harttle <yangjvn@126.com>
2017-08-01 15:57:22 +0800

    fix lint

--- src/commands/install.js
+++ src/commands/install.js
@@ -9,18 +9,15 @@ const Package = require('../package.js')
 function install (argv, errorHandler, conf) {
   var dependencies = argv
   var save = conf.save
-  var rootNode
   var rootPkg
   var installer = new Installer()
   debug('installing:', dependencies, ', save:', save)
 
   Package.load(process.cwd())
     .then(pkg => resolver.loadRoot(rootPkg = pkg))
-    .then(node => {
-      rootNode = node
-      return Promise
-        .map(dependencies, dependency => node.addDependency(dependency))
-    })
+    .then(node => Promise.map(
+      dependencies, dependency => node.addDependency(dependency))
+    )
     .then(() => TreeNode.packageList())
     .then(pkgs => installer.install(pkgs))
     .then(() => save ? rootPkg.saveDependencies() : '')
