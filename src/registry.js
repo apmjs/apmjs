04af7ab430ea9a780bb183f86d26c15f9fcd1007
harttle <harttle@harttle.com>
2017-11-02 13:32:47 +0800

    fix downloading scoped package when config not set

--- src/registry.js
+++ src/registry.js
@@ -1,26 +1,28 @@
+'use strict'
 const log = require('npmlog')
 const debug = require('debug')('apmjs:registry')
 const url = require('url')
 const npm = require('npm')
 
 function packageUrl (name) {
-  var registry = getRegistry(name)
-  var namestr = encodeURIComponent(name)
+  let registry = getRegistry(name)
+  let namestr = encodeURIComponent(name)
   // npmjs.org insists `@` rather than `%40`
   namestr = namestr.replace(/^%40/, '@')
   return url.resolve(registry, namestr)
 }
 
 function getRegistry (name) {
-  var scope = parseScope(name)
-  var configKey = scope ? `${scope}:registry` : 'registry'
-  var registry = npm.config.get(configKey)
+  let scope = parseScope(name)
+  let globalConfig = npm.config.get('registry')
+  let scopedConfig = npm.config.get(`${scope}:registry`)
+  let registry = scope ? (scopedConfig || globalConfig) : globalConfig
   log.verbose('using registry:', registry)
   return registry
 }
 
 function parseScope (name) {
-  var match = /^(@.+)\//.exec(name)
+  let match = /^(@.+)\//.exec(name)
   return match && match[1]
 }
 
